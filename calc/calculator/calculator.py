420fcb82f0b38be38719312c84f5025b1ddbd1b7
PaolinoRossi <a.cecchi8@studenti.unipi.it>
2021-10-14 18:14:30 +0200

    Code bug fixed

--- calc/calculator/calculator.py
+++ calc/calculator/calculator.py
@@ -47,9 +47,9 @@ def multiply(m, n):
 def subtract(m, n):
     result = m
     if n < 0:
-        for i in range(m):
+        for i in range(abs(n)):
             result += 1
     else:
-        for i in range(m):
+        for i in range(n):
             result -= 1
     return result
