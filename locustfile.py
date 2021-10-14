69d6314e7f17385181de1f0bf2ca4c66dea62ef0
PaolinoRossi <a.cecchi8@studenti.unipi.it>
2021-10-14 18:44:06 +0200

    Locust added

--- locustfile.py
+++ locustfile.py
@@ -1,14 +1,26 @@
 import time
 from locust import HttpUser, task, between
 
-
 class QuickstartUser(HttpUser):
-    wait_time = between(1, 2)
-
-    @task
+    wait_time = between(1, 2) # 1-2 seconds between simulated events
+    @task # defines a user task, asssociated to a microthread by locust
     def index_page(self):
-        pass
-
-    @task(3)
+        self.client.get("/") # get home page
+    
+    @task(3) # this task is 3 times likelier than the previous!
     def view_item(self):
+        for item_id in range(10):
+            self.client.get(f"/calc/sum?m={item_id}&n={42}", name="/calc/sum")
+            time.sleep(1)
+        for item_id in range(10):
+            self.client.get(f"/calc/sub?m={item_id}&n={42}", name="/calc/sub")
+            time.sleep(1)
+        for item_id in range(10):
+            self.client.get(f"/calc/mul?m={item_id}&n={42}", name="/calc/mul")
+            time.sleep(1)
+        for item_id in range(10):
+            self.client.get(f"/calc/div?m={42}&n={item_id}", name="/calc/div")
+            time.sleep(1)
+
+    def on_start(self): # init for each virtual user
         pass
